f1db7e4e53fb39f02dfe6109dec007f345cf21bd
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-06-18 16:47:33 -0300

    hardhat + create-react-app starter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +20,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    development: {
+      url: 'http://127.0.0.1:7545',
+      accounts: privateKeys.split(','),
+    },
+    rinkeby: {
+      chainId: 5,
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    },
+    goerli: {
+      chainId: 4,
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: privateKeys.split(','),
+    },
+  },
 };
